c06af61114fa742b54a528ae670cb50f72bc9852
Elijah Schow <elijah.schow@gmail.com>
2017-02-15 16:18:14 -0600

    Implement `SongQueue`
    Passes all tests for SongQueue. See `SpecRunner.html` or code comments
    for details.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -3,7 +3,37 @@ var SongQueue = Backbone.Collection.extend({
 
   model: SongModel,
 
-  initialize: function() {
-  }
+  initialize: function () {
 
+    // when as song is added
+    this.on('add', function () {
+      // play it only if it is the only song in the queue
+      if( this.length === 1 ) {
+        this.playFirst();
+      }
+      // do nothing if it is not the only song in the queue
+    }, this);
+
+    // when a song ends
+    this.on('ended', function() {
+      // remove the song from the queue
+      this.remove(this.at(0));
+      // play the first song in the queue if there are any songs left
+      if(this.length > 0) {
+        this.playFirst();
+      }
+      // do nothing if there are no songs left in the queue
+    });
+
+    // when as song is dequeued
+    this.on('dequeue', function() {
+      // remove the song
+      this.remove(this.at(0));
+    });
+
+  },
+  playFirst : function (event) {
+    // play the first song in the queue
+    this.at(0).play();
+  }
 });
