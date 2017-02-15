ad3384c95aea69524ddfd8e8a8bfb9c10ed073f5
Elijah Schow <elijah.schow@gmail.com>
2017-02-15 16:31:31 -0600

    Implement `SongQueueView`
    All tests for `SongQueueView` pass. See code comments or
    `test/spec/SongQueueViewSpec.js` for details.

--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -2,10 +2,19 @@
 var SongQueueView = Backbone.View.extend({
 
   initialize: function() {
+    // render when add or remove events fire from the song queue collection
+    this.collection.on('add', this.render, this);
   },
 
   render: function() {
+    // create SongQueueEntryViews for each queued song & render them
+    this.collection.forEach(this.renderSong, this);
     return this.$el;
+  },
+
+  renderSong: function(song) {
+    var songView = new SongQueueEntryView({model: song});
+    this.$el.append(songView.render());
   }
 
 });
