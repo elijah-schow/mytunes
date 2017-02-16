0afaf182b69e4f26d9047199955c94aa92e20752
Elijah Schow <elijah.schow@gmail.com>
2017-02-15 19:21:45 -0600

    GET song data from Parse when app initializes

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,20 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
 
-  model: SongModel
+  model: SongModel,
+
+  initialize: function() {
+    // GET song data from Parse when initialized
+    $.ajax({
+      url: config.server,
+      type: 'GET',
+      success: function(data) {
+        this.set(data.results);
+      }.bind(this),
+      error: function(data){
+        console.error('Failed to Load Song Data:');
+      }
+    });
+  }
 
 });
